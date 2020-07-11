a83c5e74122a5b6673fd51fcede8a9bf56be8cfd
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-07-10 22:50:56 -0400

    seed working

--- seed.sql
+++ seed.sql
@@ -35,10 +35,10 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
 INSERT INTO role (title, salary)
-VALUES ("Sales Lead", 60000), ("Sales Person", 40000), ("Lead Engineer", 100000), ("Software Engineer", 70000), ("Account Manager", 50000), ("Accountant", 60000), ("Legal Team Lead", 150000), ("Lawyer", 90000);
+VALUES ("Sales Lead", 60000, 1), ("Sales Person", 40000, 1), ("Lead Engineer", 100000, 2), ("Software Engineer", 70000, 2), ("Account Manager", 50000, 1), ("Accountant", 60000, 3), ("Legal Team Lead", 150000,4), ("Lawyer", 90000,4);
 
 INSERT INTO employee (first_name, last_name)
-VALUES ("Daryl", "Reedus"), ("Rick", "Grimes"), ("Negan", "Wilson"), ("Carol", "Peletier"), ("Maggie", "Green"), ("Michonne", "Hawthorne"), ("Eugene", "Porter"), ("Rosita", "Espinosa");
+VALUES ("Daryl", "Reedus",1,null), ("Rick", "Grimes",2,1), ("Negan", "Wilson",3,null), ("Carol", "Peletier",4,3), ("Maggie", "Green",5,1), ("Michonne", "Hawthorne",6,null), ("Eugene", "Porter",7,null), ("Rosita", "Espinosa",8,7);
 
 
 SELECT * FROM department;
